a3bfcf028b0f8236a4f39d2032b933d3af7fc349
Jakhongir <Jakhongir45@gmail.com>
2022-01-22 18:20:23 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
+ 
+  function inc() {
+    setCount(prev => prev + 1)
+  }
+  function dec() {
+    setCount(prev => prev - 1)
+  }
+
   return (
-    <h1>Hello world</h1>
+    <>
+      <h3>Count: {count}</h3>
+      <button 
+      onClick={inc}
+      className="btn btn-success">Incr</button>
+      <button 
+      onClick={dec}
+      className="btn btn-danger">Decr</button>  
+    </>    
   );
 }
 
