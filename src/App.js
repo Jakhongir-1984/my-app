f7b8cab5b3a7029c517dfec84f90bcda3f658c2e
Jakhongir <Jakhongir45@gmail.com>
2022-02-01 19:17:17 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, {useState} from "react";
-import "./style/style.css"
-import Counter from "./components/Counter";
-import InputVal from "./components/InputVal";
-import ToggleBtn from "./components/ToggleBtn";
+import React, { useState } from "react";
+import TableHeader from "./components/TableHeader";
+import TableItem from "./components/TableItem";
+import "./style/styles.css"
+
 
 function App() {
-  
+  const [posts, setPosts] = useState([
+    {id: 1, title: "JavaScript", stack: "MERN Stack"},
+    {id: 2, title: "Python", stack: "Full-Stack"},
+    {id: 3, title: "C#", stack: "Game"},
+    {id: 4, title: "PHP", stack: "Back End"},
+  ])
+
   return (
-    <>
-      <div className="app w-50 mx-auto">
-        <Counter />
-        <hr/>
-        <InputVal />
-        <ToggleBtn />
-      </div>  
-    </>    
+    <div className="app w-50 mx-auto">
+      <table className="table table-striped"> 
+        <TableHeader />
+        <tbody>
+        {posts.map(post => (
+          <TableItem post={post} />
+        ))} 
+        </tbody>
+      </table>
+    </div>    
   );
 }
 
---
+++ src/components/TableHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TableHeader = () => {
+  return (
+      <thaed>
+        <tr>
+          <th>#</th>
+          <th>Programming</th>
+          <th>Stack</th>
+          <th>Actions</th>
+        </tr>
+      </thaed>
+  );
+};
+
+export default TableHeader;
