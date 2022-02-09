b3ba6034ef773e7c2d90635c8d157a0c30961cb4
Jakhongir <Jakhongir45@gmail.com>
2022-02-09 11:51:29 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React, {useState, useRef} from "react";
+import React, {useState} from "react";
 import "./style/styles.css"
 import TableList from "./components/TableList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
 
 function App() {
@@ -24,7 +22,16 @@ function App() {
   return (
     <div className="app w-50 mx-auto">
       <PostForm createPost={createPost}/>
-      <TableList remove={removePost} posts={posts} title="Programming Language" />
+      <div className="d-flex flex-row-reverse my-2">
+        <select className="form-select w-50">
+          <option value="val">Sorted by Title</option>
+          <option value="val">Sorted by Job</option>
+        </select>  
+      </div>
+      {posts.length
+      ? <TableList remove={removePost} posts={posts} title="Programming Language" />
+      : <h6 className="my-3 text-center">You should add some Post</h6>
+      }  
     </div> 
   );
 }
